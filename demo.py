3c49710f65fc352e09bd96c9516a4c23e5e0cadf
Vinoth <Vino92babu@gmail.com>
2026-06-17 12:21:33 +0530

    revome

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install())  jb
+    service=Service(ChromeDriverManager().install())
 )
 
 driver.maximize_window()
