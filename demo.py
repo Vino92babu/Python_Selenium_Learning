08640fd5aa2c90aedc38a1614efb1616116b504b
Vinoth <Vino92babu@gmail.com>
2026-06-17 12:18:04 +0530

    aa

---
+++ demo.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install())  jb
+)
+
+driver.maximize_window()
+driver.get("https://www.google.com")
+
+print("Title:", driver.title)
+
+driver.quit()
