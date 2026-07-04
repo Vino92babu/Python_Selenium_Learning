d37329824221c2326926a76ae67c4a1f48e864cb
vino92babu <vino92babu@gmail.com>
2026-07-05 01:00:20 +0530

    UI_Control--> dropdown

--- Selenium/UI_Controls.py
+++ Selenium/UI_Controls.py
@@ -1,12 +1,16 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+
+from selenium.webdriver.support.select import Select
+
 driver=webdriver.Chrome()
-driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+'''driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 driver.maximize_window()
 #Check box
 #Static check box
-# driver.find_element(By.XPATH,'//input[@id="checkBoxOption2"]').click()
+driver.find_element(By.XPATH,'//input[@id="checkBoxOption2"]').click()
 # Dynamic check box- when it changes its position
 chkbx = driver.find_elements(By.XPATH,'//input[@type="checkbox"]')
 print(len(chkbx))
@@ -17,14 +21,23 @@ for checkbox in chkbx:
 
         print(checkbox.get_attribute("value"))
         break
-
+'''
 # Radio Button
 # Static Radio button
-driver.find_element()
+driver.get("https://www.sreenidhirajakrishnan.com/practice?utm_source=sp_auto_dm&utm_referrer=sp_auto_dm&fbclid=PAT01DUASvkt1leHRuA2FlbQIxMABzcnRjBmFwcF9pZA81NjcwNjczNDMzNTI0MjcAAaecrzPgG4Ywp8o5SYBeclsS7CPfX2WGpd8T0HWsnqHvGe8jgUb91LcAIF1nzg_aem_hPrp5rPeLzIUNLz60JBw9g")
+driver.maximize_window()
+# radio_option=driver.find_elements(By.XPATH,"//select[@id='standard-select']")
+# print(len(radio_option))
+# for option in radio_option:
+#     if option.get_attribute("value") == "Green":
+#         option.click()
+#         print(option.get_attribute("value"))
+#         break
 
+dropdown = Select(driver.find_element(By.ID, "standard-select"))
 
-
-
-
-
+dropdown.select_by_visible_text("Red")
 time.sleep(2)
+dropdown.select_by_value("blue")
+message = driver.find_element(By.ID, "standard-select").text
+
