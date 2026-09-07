0331339edcb7a44251b98bd4027bcaa6923f918b
Vino <vino92babu@gmail.com>
2026-09-07 17:49:14 +0530

    pytest

---
+++ pytestDemo/test_demo.py
@@ -0,0 +1,7 @@
+def test_sample():
+    print("ABC Hey hiiiiii") 
+
+
+def test_sample1():
+    print("Sample")
+
---
+++ pytestDemo/test_demo2.py
@@ -0,0 +1,7 @@
+def test_demo001():
+    A = "Vinoth"
+    assert A == "Babu", "This is worng name"
+
+
+def test_sample2():
+    print("Sample_test")
